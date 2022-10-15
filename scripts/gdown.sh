593609f8fed9c86543926be7a9b8808e9185b306
willgpaik <bgh0919@gmail.com>
2022-10-15 05:13:39 -0400

    updated for Banana Pi M2 Zero

---
+++ scripts/gdown.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# install gdown PyPI module:
+apt install -y python pip
+pip install gdown
+
+# download bpi m2 zero image (pi OS stretch):
+gdown --id 1G5NJDwL0Vs9YLwUEdGXMSwBPqRx6CxTC
+
